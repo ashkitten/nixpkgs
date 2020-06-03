912d0fc8e137d11e93dac55bbd22393bee24b535
ash lea <example@thisismyactual.email>
2020-06-03 14:10:40 -0700

    libshout: add patches to fix icecast streaming

--- pkgs/development/libraries/libshout/default.nix
+++ pkgs/development/libraries/libshout/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, pkgconfig
+{ stdenv, fetchurl, pkgconfig, fetchpatch
 , libvorbis, libtheora, speex }:
 
 # need pkgconfig so that libshout installs ${out}/lib/pkgconfig/shout.pc
@@ -10,6 +10,18 @@ stdenv.mkDerivation rec {
     url = "http://downloads.xiph.org/releases/libshout/${name}.tar.gz";
     sha256 = "1zhdshas539cs8fsz8022ljxnnncr5lafhfd1dqr1gs125fzb2hd";
   };
+
+  patches = [
+    # these 2 fix https://gitlab.xiph.org/xiph/icecast-libshout/-/issues/2308, can be removed on next release
+    (fetchpatch {
+      url = "https://gitlab.xiph.org/xiph/icecast-libshout/-/commit/0ac7ed9e84c3871d4427acc1ce59dca5e4af21ef.diff";
+      sha256 = "14ha9zajhpdzfgzhj8hl5rhdrczrmmm9klwp239hs0drxj674r43";
+    })
+    (fetchpatch {
+      url = "https://gitlab.xiph.org/xiph/icecast-libshout/-/commit/b807c1e2550718bdc73d65ac1b05255d18f45c54.diff";
+      sha256 = "0cps64396cnml66kg7czfd2vas04nn5nhj4l0qll9jdx5q078ypa";
+    })
+  ];
 
   outputs = [ "out" "dev" "doc" ];
 
